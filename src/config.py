9b3a3792ee7e64933b2b6aaa22192ebf072cdefa
arjj8 <arjj@dmi.dk>
2025-12-10 10:49:21 +0100

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+RADAR_COMPOS = Path("/dmidata/archive/arch1/home/radman/products/composite")
+RADAR_ZARR = Path("/dmidata/projects/radar/products/composite/zarrComposite/products/")
+RADAR_ZTEMP = Path("/dmidata/projects/radar/products/composite/zarrComposite/tmp_folder")
+
+init_year = '2016'
+final_year = '2024'
+is_new = 0
+
+SpatialRes500 = {Xpixels: 1984,Ypixels:1728}
+SpatialRes1000 = {Xpixels: 992,Ypixels:864}
+
